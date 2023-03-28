561d7ad03b78da9134db54dc64d5f42627808949
Gabrielk127 <gabrielfsilva09@gmail.com>
2023-03-28 20:09:39 -0300

    update1

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const btHome = document.querySelector('#home')
+const btWelcome = document.querySelector('#btWelcome')
+const btMenu = document.querySelector('#menu')
+
+const welcome = document.querySelector('.welcome')
 const page1 = document.querySelector('.page1')
 const page2 = document.querySelector('.page2')
 
-btHome.addEventListener('click', handleClick)
+
+
+btWelcome.addEventListener('click', handleClick)
+btMenu.addEventListener('click', handleClick)
 
 function handleClick() {
     page1.classList.toggle('hide')
-    page2.classList.toggle('hide')
+    welcome.classList.toggle('hide')
 }
+
