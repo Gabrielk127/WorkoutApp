a572d4a7c367c29cc8fe877f7714ef407b0321a6
Gabrielk127 <gabrielfsilva09@gmail.com>
2023-03-29 21:54:56 -0300

    update 29/03/2023

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
-const btHome = document.querySelector('#home')
+// BOTÕES
 const btWelcome = document.querySelector('#btWelcome')
-const btMenu = document.querySelector('#menu')
-const btCarrosel1 = document.querySelector('#handle1')
-const btCarrosel2 = document.querySelector('#handle2')
-const btCarrosel3 = document.querySelector('#handle3')
+const btQuadriceps = document.querySelector('#btQuadriceps')
+const btSuperior = document.querySelector('#btSuperior')
+const btPosterior = document.querySelector('#btPosterior')
 
+
+// PAGINAS
 const welcome = document.querySelector('.welcome')
 const page1 = document.querySelector('.page1')
 const page2 = document.querySelector('.page2')
 const page3 = document.querySelector('.page3')
 const page4 = document.querySelector('.page4')
 
+// EVENTOS
 
-btWelcome.addEventListener('click', handleClick)
-btMenu.addEventListener('click', handleClick)
-btCarrosel1.addEventListener('click', handleClick1)
-btCarrosel2.addEventListener('click', handleClick2)
-btCarrosel3.addEventListener('click', handleClick3)
+btWelcome.addEventListener('click', handleClickPage1)
+btQuadriceps.addEventListener('click', handleClickPage2)
+btSuperior.addEventListener('click', handleClickPage3)
+btPosterior.addEventListener('click', handleClickPage4)
 
-function handleClick3() {
-    page1.classList.toggle('hide')
-    page4.classList.toggle('hide')
-}
+// FUNÇÕES
 
-function handleClick2() {
-    page1.classList.toggle('hide')
-    page3.classList.toggle('hide')
-}
-
-function handleClick1() {
-    page1.classList.toggle('hide')
-    page2.classList.toggle('hide')
-}
-
-function handleClick() {
+function handleClickPage1() {
     page1.classList.toggle('hide')
     welcome.classList.toggle('hide')
 }
 
+function handleClickPage2() {
+    page1.classList.toggle('hide')
+    page2.classList.toggle('hide')
+}
+
+function handleClickPage3() {
+    page1.classList.toggle('hide')
+    page3.classList.toggle('hide')
+}
+
+function handleClickPage4() {
+    page1.classList.toggle('hide')
+    page4.classList.toggle('hide')
+}
